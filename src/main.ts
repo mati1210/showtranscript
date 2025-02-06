0c7102265c8bc26d5a0a05c7efd26af193b37591
mat1210 <matheuz1210@gmail.com>
2025-02-06 04:44:48 -0300

    wip

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MPL-2.0
+
+import Speakers from './speakers.js';
+const filepicker: HTMLInputElement = document.getElementById("filepicker") as HTMLInputElement
+const renderarea = document.getElementById("renderarea") as HTMLDivElement
+const speakerstable = document.getElementById("speakerstable") as HTMLTableSectionElement
+
+const speakers: Speakers = new Speakers()
+speakers.onchange = () => {
+    speakers.render(speakerstable)
+}
+
+filepicker.addEventListener("change", (ev) => {
+    if (filepicker.files == null) return
+    for (const file of filepicker.files) {
+        console.log(file)
+        speakers.add(file)
+    }
+})
---
+++ src/speakers.ts
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MPL-2.0
+
+interface Segment {
+    start: number,
+    end: number,
+    text: string
+}
+interface Speaker {
+    name: string
+    color: string
+    segments: Segment[]
+}
+
+export default class Speakers {
+    speakers: Speaker[] = []
+    onchange: () => void = () => { }
+
+    add(file: File) {
+        const name = file.name.endsWith(".json") ? file.name.slice(0, -5) : file.name
+        const reader = new FileReader()
+
+        reader.onload = ev => {
+            try {
+                const data = JSON.parse(ev.target!.result!.toString())
+                this.speakers.push({ name, segments: data["segments"], color: "white" })
+            } catch (error) {
+                console.error(error)
+                alert("failed to parse file!")
+                return
+            }
+            finally { this.onchange() }
+        }
+        reader.readAsText(file)
+    }
+
+    render(table: HTMLTableSectionElement) {
+        table.innerHTML = ""
+
+        for (const speaker of this.speakers) {
+            let row = table.insertRow();
+            console.log(row)
+
+            let nameInput = document.createElement("textarea")
+            nameInput.rows = 1;
+            nameInput.value = speaker.name
+            nameInput.onchange = () => { speaker.name = nameInput.value; this.onchange() }
+            let name = row.insertCell()
+            name.appendChild(nameInput)
+
+            let colorInput = document.createElement("input");
+            colorInput.type = "color";
+            colorInput.value = speaker.color;
+            colorInput.onchange = () => { speaker.color = colorInput.value; this.onchange() }
+            let colour = row.insertCell()
+            colour.appendChild(colorInput)
+
+            let removeInput = document.createElement("button")
+            removeInput.innerText = "X"
+            removeInput.style.color = "red"
+            removeInput.onclick = () => {
+                row.remove()
+                const idx = this.speakers.indexOf(speaker)
+                if (idx > -1) {
+                    this.speakers.splice(idx, 1)
+                }
+                this.onchange()
+            }
+            let remove = row.insertCell()
+            remove.appendChild(removeInput)
+        }
+
+    }
+}
